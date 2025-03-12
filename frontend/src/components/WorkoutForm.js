7180eb8f4369714419abe51e6c23ca3d9b2cfc74
Yadnyeshhh <yadnyesh1024@gmail.com>
2025-03-12 17:15:29 +0530

    Update WorkoutForm.js

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -16,7 +16,7 @@ const WorkoutForm = () => {
       return;
     }
     const workout = { title, load, reps };
-    const response = await fetch("/api/workouts", {
+    const response = await fetch("https://workoutbuddy-backend-aj5e.onrender.com/api/workouts", {
       method: "POST",
 
       body: JSON.stringify(workout),
